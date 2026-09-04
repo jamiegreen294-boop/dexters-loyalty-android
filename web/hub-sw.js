c381065e771b9010b95e55177539b4a80496f574
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-04 12:00:24 +0100

    Secure permanent Dexter's Hub with owner authentication

--- web/hub-sw.js
+++ web/hub-sw.js
@@ -1,4 +1,4 @@
-const CACHE='dexters-hub-test-v3';
+const CACHE='dexters-hub-test-v4';
 const ASSETS=['/hub-test.html','/hub-test.css','/hub-test.js','/hub-manifest.webmanifest','/hub-admin-test.html','/hub-admin-test.css','/hub-admin-test.js','/accounts-test.html','/accounts-test.css','/accounts-test.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
