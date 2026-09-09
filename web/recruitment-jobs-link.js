6a40ead296e0f706e53b122644027ddadd5d3012
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-09 15:31:24 +0100

    Trigger complete recruitment jobs flow build

--- web/recruitment-jobs-link.js
+++ web/recruitment-jobs-link.js
@@ -1,3 +1,4 @@
+/* DEXTERS_RECRUITMENT_JOBS_FLOW_V2 */
 (function(){
  if(window.__dextersJobsLinkReady)return;window.__dextersJobsLinkReady=true;
  const JOBS='https://jamiegreen294-boop.github.io/dexters-recruitment-test/jobs.html';
