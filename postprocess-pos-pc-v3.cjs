e28dce800ca8b7302eb856e8c83b43c49d8a4b5d
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-18 18:03:32 +0100

    Fix generated POS inline status JavaScript

--- postprocess-pos-pc-v3.cjs
+++ postprocess-pos-pc-v3.cjs
@@ -16,7 +16,7 @@ const buildId=require('crypto').createHash('sha256').update(pinBootstrap).update
 html=html.replace('</head>','<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">\n<script>window.__DEXTERS_POS_BUILD='+JSON.stringify(buildId)+'</script>\n</head>');
 html=html.replace('</body>','<script>'+pinBootstrap+'</script>\n</body>');
 html=html.replace("Live POS · connected to Dexter's order system.","LIVE POS · connected Dexter's system");
-html=html.replaceAll("Menu loaded · Live POS","Menu loaded · Dexter's POS");
+html=html.replaceAll("Menu loaded · Live POS","Menu loaded · Dexter’s POS");
 html=html.replace("DEXTER'S · TABLE SERVICE","DEXTER'S POS · LIVE");
 fs.writeFileSync('dist/pos.html',html);
 fs.writeFileSync('dist/pos-test.html',html);
