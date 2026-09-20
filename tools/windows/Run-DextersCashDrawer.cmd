1dca01a135491c7f2e14ce0408e15e539e4c02c4
jamiegreen294-boop <jamiegreen294@gmail.com>
2026-09-20 14:38:06 +0100

    Make cash drawer launcher self-contained

--- tools/windows/Run-DextersCashDrawer.cmd
+++ tools/windows/Run-DextersCashDrawer.cmd
@@ -1,6 +1,6 @@
 @echo off
 title Dexter Cash Drawer Test
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Test-DextersCashDrawer.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/jamiegreen294-boop/dexters-loyalty-android/dexters-pos-pc-test-v3/tools/windows/Test-DextersCashDrawer.ps1')"
 echo.
 echo Press any key to close...
 pause >nul
